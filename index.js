f031466d50b56fb6a8881090976009dddf55da69
HappyFeet3334 <128722231+HappyFeet3334@users.noreply.github.com>
2024-01-02 21:33:47 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -21,3 +21,14 @@ function moveButton() {
         absolutePositionSet = true;
     }
 }
+
+function yippee() {
+    var text = document.getElementById('main heading');
+    var uniqueElement = document.getElementById('movingButton');
+    var image = document.getElementById('main img');
+    var button = document.getElementById('yes');
+    text.textContent = 'yippee! I like you too :-)';
+    image.src = 'https://media.istockphoto.com/id/1307507771/photo/two-small-striped-domestic-kittens-sleeping-hugging-each-other-at-home-lying-on-bed-white.jpg?s=612x612&w=0&k=20&c=3k2NzD2a9jFZkw86kVo2Te6cNhev38AQ95FxR9fFeXU=';
+    uniqueElement.hidden = true;
+    button.hidden = true;
+}   
